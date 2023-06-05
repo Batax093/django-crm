def099943cda07ac3930c56a3ac8ff8e34bad421
Batax093 <lifeisggwp18@gmail.com>
2023-06-06 00:10:41 +0700

    settings

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -100,11 +100,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
