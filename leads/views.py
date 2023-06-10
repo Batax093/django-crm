3e7ca0bffd9779ffcb6f2623e09684b527bb27d7
Batax093 <lifeisggwp18@gmail.com>
2023-06-10 21:49:30 +0700

    crsf

--- leads/views.py
+++ leads/views.py
@@ -16,12 +16,8 @@ class SignupView(generic.CreateView):
     def get_success_url(self):
         return reverse("login")
 
-
 class LandingPageView(generic.TemplateView):
     template_name = "landing_page.html"
-
-def landing_page(request):
-    return render(request, 'landing_page.html')
 
 class LeadListView(LoginRequiredMixin, generic.ListView):
     template_name = "leads/leads_list.html"
@@ -50,14 +46,6 @@ class LeadListView(LoginRequiredMixin, generic.ListView):
             })
         return context
 
-def leads_list(request):
-    leads = Lead.objects.all()
-    context = {
-        "leads": leads
-    }
-
-    return render(request, 'leads_list.html', context)
-
 class LeadDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "leads/lead_detail.html"
     context_object_name = 'lead'
@@ -72,15 +60,6 @@ class LeadDetailView(LoginRequiredMixin, generic.DetailView):
 
             queryset = queryset.filter(agent__user=user)
         return queryset
-
-def lead_detail(request, pk):
-    print(pk)
-    lead = Lead.objects.get(id=pk)
-    context = {
-        "lead": lead
-    }
-
-    return render(request, 'lead_detail.html', context)
 
 class LeadCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
     template_name = "leads/lead_create.html"
@@ -102,21 +81,6 @@ class LeadCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         )
         return super(LeadCreateView, self).form_valid(form)
 
-def lead_create(request):
-    form = LeadModelForm()
-    if request.method == "POST":
-        form = LeadModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            print("The lead has been created")
-            return redirect("/leads")
-
-    context = {
-        "form": form 
-    }
-
-    return render(request, 'lead_create.html', context)
-
 class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     template_name = "leads/lead_update.html"
     form_class = LeadModelForm
@@ -128,23 +92,6 @@ class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     def get_success_url(self):
         return reverse("leads:lead-list")
 
-def lead_update(request, pk):
-    lead = Lead.objects.get(id=pk)
-
-    form = LeadModelForm(instance=lead)
-    if request.method == "POST":
-        form = LeadModelForm(request.POST, instace = lead)
-        if form.is_valid():
-            lead.save()
-            return redirect('/leads')
-
-    context = {
-        "lead": lead,
-        "form": form
-    }
-    
-    return render(request, 'lead_update.html', context)
-
 class LeadDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
     template_name = "leads/lead_delete.html"
 
@@ -154,11 +101,6 @@ class LeadDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
     def get_queryset(self):
         user = self.request.user
         return Lead.objects.filter(organisation=user.userprofile)
-
-def lead_delete(request, pk):
-    lead = Lead.objects.all()
-    lead.delete()
-    return redirect("/leads")
 
 class AssignAgentView(OrganisorAndLoginRequiredMixin, generic.FormView):
     template_name = "assign_agent.html"
