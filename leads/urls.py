c3e2b0c275b0e887f690689d3271ed887b965182
Batax093 <lifeisggwp18@gmail.com>
2023-06-10 22:01:52 +0700

    urls

--- leads/urls.py
+++ leads/urls.py
@@ -19,9 +19,9 @@ urlpatterns = [
     path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
-    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
-    path('<int:pk>/category/', LeadCategoryUpdateView.as_view(), name='lead-category-update'),
+    path('<int:pk>/assign-customer/', AssignAgentView.as_view(), name='assign-agent'),
+    path('<int:pk>/tugas/', LeadCategoryUpdateView.as_view(), name='lead-category-update'),
     path('create/', LeadCreateView.as_view(), name='lead-create'),
-    path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('list-tugas/', CategoryListView.as_view(), name='category-list'),
+    path('list-tugas/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 ]
